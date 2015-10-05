d077d60d0020f1706a7a48d53f7cd40aa560a156
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2015-10-05 17:13:09 +0530

    pin version to 0.1.0 for public release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from setuptools import setup, find_packages
 
 __name__ = 'superelasticsearch'
-__version__ = '0.4.4'
+__version__ = '0.1.0'
 __author__ = 'Vaidik Kapoor'
 __author_email__ = 'vaidik.kapoor@wingiify.com'
 
